2b93ef380d12103e5f9b375f5eb3fbec9d8d7b04
Yuki M <yukiji.0307@gmail.com>
2020-08-01 16:33:50 +0900

    create Github app

---
+++ ajaxapp/index.js
@@ -0,0 +1,43 @@
+// mainの関数
+const searchUserId = async () => {
+  try {
+    const userId = document.getElementById('userId').value
+    const userInfo = await fetchUserInfo(userId)
+    const view = createView(userInfo)
+    displayView(view)
+  } catch (error) {
+    const errorView = `エラーが発生しました(${error})`
+    displayView(errorView)
+  }
+}
+
+// githubのuser_idを取得する
+// fetchはPromiseを返す
+const fetchUserInfo = (userId) => {
+  return fetch(
+    `https://api.github.com/users/${encodeURIComponent(userId)}`
+  ).then((res) => {
+    if (!res.ok) {
+      return Promise.reject(new Error(`${res.status}: ${res.statusText}`))
+    } else {
+      return res.json()
+    }
+  })
+}
+
+// user情報を受け取ってHTMLを組み立てる
+const createView = (userInfo) => {
+  return `
+            <h4>${userInfo.name} (@${userInfo.login})</h4>
+            <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+            <ul>
+                <li>Location：${userInfo.location}</li>
+                <li>Repositories：${userInfo.public_repos}</li>
+            </ul>`
+}
+
+// id="result"に結果を描画する
+const displayView = (view) => {
+  const result = document.getElementById('result')
+  result.innerHTML = view
+}
---
+++ ajaxapp/readme.md
@@ -0,0 +1,12 @@
+# setup
+
+```
+npx @js-primer/local-server
+```
+
+# study
+
+- Promise
+- fetch
+- async
+- await
