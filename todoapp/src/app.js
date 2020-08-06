7e857e375ed9071f91be5ca5141fb1527bb46876
松本雄揮 <yukiji.0307@gmail.com>
2020-08-06 13:24:01 +0900

    todo app create

--- todoapp/src/app.js
+++ todoapp/src/app.js
@@ -1,19 +1,62 @@
-import { element } from './view/html-utils.js'
+import { render } from './view/html-util.js'
+import { TodoListView } from './view/TodoListView.js'
+import { TodoItemModel } from './model/TodoItemModel.js'
+import { TodoListModel } from './model/TodoListModel.js'
 
 export class App {
+  constructor() {
+    this.todoListView = new TodoListView()
+    this.todoListModel = new TodoListModel([])
+  }
+
+  /**
+   * Todoを追加するときに呼ばれるリスナー関数
+   * @param {string} title
+   */
+  handleAdd(title) {
+    this.todoListModel.addTodo(new TodoItemModel({ title, completed: false }))
+  }
+
+  /**
+   * Todoの状態を更新したときに呼ばれるリスナー関数
+   * @param {{ id:number, completed: boolean }}
+   */
+  handleUpdate({ id, completed }) {
+    this.todoListModel.updateTodo({ id, completed })
+  }
+
+  /**
+   * Todoを削除したときに呼ばれるリスナー関数
+   * @param {{ id: number }}
+   */
+  handleDelete({ id }) {
+    this.todoListModel.deleteTodo({ id })
+  }
+
   mount() {
     const formElement = document.querySelector('#js-form')
     const inputElement = document.querySelector('#js-form-input')
-    const containerElement = document.querySelector('#js-todo-list')
     const todoItemCountElement = document.querySelector('#js-todo-count')
+    const containerElement = document.querySelector('#js-todo-list')
+    this.todoListModel.onChange(() => {
+      const todoItems = this.todoListModel.getTodoItems()
+      const todoListElement = this.todoListView.createElement(todoItems, {
+        // Appに定義したリスナー関数を呼び出す
+        onUpdateTodo: ({ id, completed }) => {
+          this.handleUpdate({ id, completed })
+        },
+        onDeleteTodo: ({ id }) => {
+          this.handleDelete({ id })
+        },
+      })
+      render(todoListElement, containerElement)
+      todoItemCountElement.textContent = `Todoアイテム数: ${this.todoListModel.getTotalCount()}`
+    })
 
-    let todoItemCount = 0
     formElement.addEventListener('submit', (event) => {
       event.preventDefault()
-      const todoItemElement = element`<li>${inputElement.value}</li>`
-      containerElement.appendChild(todoItemElement)
-      todoItemCount += 1
-      todoItemCountElement.textContent = `Todoアイテム数： ${todoItemCount}`
+      if (!inputElement.value) return
+      this.handleAdd(inputElement.value)
       inputElement.value = ''
     })
   }
